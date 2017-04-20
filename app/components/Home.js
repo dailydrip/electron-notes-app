11b93ce4de43b02f20f0eb52785a9408040c94e7
Franzé Jr <franzejr@gmail.com>
2017-04-20 18:35:04 -0300

    Initial version of the UI - without Notifications and menu

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,11 +3,86 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Home.css';
 
+
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <div className={styles.container} data-tid="container">
+      <div className="window">
+        <div className="window-content">
+          <div className="pane-group">
+            <div className="pane-sm sidebar">
+
+              <ul className="list-group">
+                <li className="list-group-header">
+                  <input className="form-control" type="text" placeholder="Search for some Note" />
+                </li>
+                <li className="list-group-item">
+                  <div className="media-body">
+                    <strong>List item title</strong>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <div className="media-body">
+                    <strong>List item title</strong>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <div className="media-body">
+                    <strong>List item title</strong>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <div className="media-body">
+                    <strong>List item title</strong>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <div className="media-body">
+                    <strong>List item title</strong>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <div className="media-body">
+                    <strong>List item title</strong>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <div className="media-body">
+                    <strong>List item title</strong>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <div className="media-body">
+                    <strong>List item title</strong>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div className="pane">
+              <form>
+                <div className="form-group">
+                  <label>Title</label>
+                  <input type="text" className="form-control" placeholder="Title" />
+                </div>
+                <div className="form-group">
+                  <label>Description</label>
+                  <textarea className="form-control" rows="20" cols="10"></textarea>
+                </div>
+                <div class="form-actions">
+                  <button type="submit" className="btn btn-form btn-default">DISCARD</button>
+                  <button type="submit" className="btn btn-form btn-primary">SAVE</button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     );
